ea8eed83d01abedee5ea578fe86a3592149e500c
Michael Zhou <michaelzhou24@gmail.com>
2018-06-23 17:19:11 -0700

    added tableView and working on other viewcontrollers

--- Emojis/ViewController.swift
+++ Emojis/ViewController.swift
@@ -8,10 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var mainTableView: UITableView!
+    var emojis = ["😡","😂","😉","😘","🙃","😑"]
     override func viewDidLoad() {
         super.viewDidLoad()
+        mainTableView.dataSource = self
+        mainTableView.delegate = self
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +24,16 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    // Number of rows in the table
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+    }
+    
+    // Content in the table
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
+    }
 }
 
